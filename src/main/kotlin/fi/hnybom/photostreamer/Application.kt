f6b7b62458971806ffc743ce60aa7e8996d7bb8e
Henri Nybom <henri.nybom@gmail.com>
2016-06-14 20:36:04 +0300

    Images are now server through the server.

--- src/main/kotlin/fi/hnybom/photostreamer/Application.kt
+++ src/main/kotlin/fi/hnybom/photostreamer/Application.kt
@@ -2,23 +2,35 @@ package fi.hnybom.photostreamer
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 
 /**
  * Created by hnybom on 8.6.2016.
  */
 
+val rootFolder = "/volume1/Verkkolevy/sync/Google valokuvat"
+val rootFolder2 = "/Volumes/Verkkolevy/sync/Google valokuvat"
+
 @SpringBootApplication
-open class Application {
+open class Application : WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter() {
 
     @Bean
     open fun objectMapperBuilder(): Jackson2ObjectMapperBuilder
             = Jackson2ObjectMapperBuilder().modulesToInstall(KotlinModule())
 
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry?) {
+
+        if(registry != null) {
+            registry.addResourceHandler("/images/**")
+                    .addResourceLocations("file:" + fi.hnybom.photostreamer.rootFolder2 + "/")
+        }
+        super.addResourceHandlers(registry)
+    }
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/fi/hnybom/photostreamer/service/FileSystemSource.kt
+++ src/main/kotlin/fi/hnybom/photostreamer/service/FileSystemSource.kt
@@ -1,8 +1,5 @@
 package fi.hnybom.photostreamer.service
 
-import com.drew.imaging.ImageMetadataReader
-import com.drew.metadata.exif.ExifIFD0Directory
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import java.io.File
 import java.util.*
@@ -16,21 +13,15 @@ data class Photo(val name: String, val timestamp: Date, val path: String)
 @Service
 class FileSystemSource {
 
-    private val rootFolder = "/volume1/Verkkolevy/sync/Google valokuvat"
-
-    private val rootFolder2 = "/Volumes/Verkkolevy/sync/Google valokuvat"
-
     val emptyPhoto = Photo("", Date(), "")
 
     val photos: List<Photo>
 
     init {
-        System.out.println("Init")
-        val walker = File(rootFolder2).walkTopDown()
+        val walker = File(fi.hnybom.photostreamer.rootFolder2).walkTopDown()
         val mappedPhotos = walker.map(fun(file): Photo {
             if(file.name.endsWith("jpg", true) || file.name.endsWith("jpeg", true)) {
-                System.out.println(file);
-                return Photo(file.name, Date(file.lastModified()), file.absolutePath)
+                return Photo(file.name, Date(file.lastModified()), "/images/" + file.absolutePath.substring(fi.hnybom.photostreamer.rootFolder2.length))
             }
             return emptyPhoto
         })
